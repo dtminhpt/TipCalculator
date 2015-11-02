08c2aadc9eca4b8f8076923c9c3f2be8e84c79f8
Dinh Thi Minh <dtminhpt@gmail.com>
2015-11-02 20:29:49 +0700

    Code for tip calculator

---
+++ TipCal1/SettingsViewController.swift
@@ -0,0 +1,87 @@
+//
+//  SettingsViewController.swift
+//  TipCal1
+//
+//  Created by Dinh Thi Minh on 11/2/15.
+//  Copyright © 2015 Dinh Thi Minh. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    let userDefaults  = NSUserDefaults.standardUserDefaults()
+   
+    @IBOutlet weak var segColour: UISegmentedControl!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let tipIndex = (userDefaults.integerForKey("ind"))
+        tipControl.selectedSegmentIndex = tipIndex
+       
+        // Load new theme
+        let bgColorNo = (userDefaults.integerForKey("bgColour"))
+        switch (bgColorNo) {
+        case 0:
+            self.view.backgroundColor = UIColor.redColor()
+        case 1:
+            self.view.backgroundColor = UIColor.blueColor()
+        case 2:
+            self.view.backgroundColor = UIColor.greenColor()
+        default:
+            self.view.backgroundColor = UIColor.redColor()
+        }
+        segColour.selectedSegmentIndex = bgColorNo
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    @IBAction func actPercentage(sender: AnyObject) {
+        let tipPercentages = [0.1, 0.15, 0.2]
+        
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        let index = tipControl.selectedSegmentIndex
+        
+        //Save
+        userDefaults.setObject(tipPercentage, forKey: "tip")
+        userDefaults.setInteger(index, forKey: "ind")
+
+    }
+    
+    
+    @IBAction func actColour(sender: UISegmentedControl) {
+        switch (segColour.selectedSegmentIndex) {
+        case 0:
+            self.view.backgroundColor = UIColor.redColor()
+        case 1:
+            self.view.backgroundColor = UIColor.blueColor()
+        case 2:
+            self.view.backgroundColor = UIColor.greenColor()
+        default:
+            self.view.backgroundColor = UIColor.redColor()
+        }
+        userDefaults.setInteger(segColour.selectedSegmentIndex, forKey: "bgColour")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TipCal1/ViewController.swift
@@ -0,0 +1,185 @@
+//
+//  ViewController.swift
+//  TipCal1
+//
+//  Created by Dinh Thi Minh on 10/30/15.
+//  Copyright © 2015 Dinh Thi Minh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    let userDefaults  = NSUserDefaults.standardUserDefaults()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+
+        //set focus to bill field
+        billField.becomeFirstResponder()
+        
+        //Load new percentage
+        let billAmount: Double! = (userDefaults.objectForKey("bill"))!.doubleValue as Double
+        let tipPercentage: Double! = (userDefaults.objectForKey(("tip"))!.doubleValue) as Double
+        let tipIndex = (userDefaults.integerForKey("ind"))
+        print(tipPercentage)
+        
+        
+        if(billAmount == nil){
+            billField.text = "100"
+            tipLabel.text = "10"
+            totalLabel.text = "110"
+            tipControl.selectedSegmentIndex = 0
+            userDefaults.setObject(billAmount, forKey: "bill")
+        }
+        
+        billField.text = String(format:"%.1f", billAmount)
+        let tip = billAmount * tipPercentage
+        let total = billAmount + tip
+        tipControl.selectedSegmentIndex = tipIndex
+        tipLabel.text = String(format:"$%.2f", tip)
+        totalLabel.text = String(format:"$%.2f", total)
+        
+        //Load new theme
+        let bgColorNo = (userDefaults.integerForKey("bgColour"))
+        switch (bgColorNo) {
+        case 0:
+            self.view.backgroundColor = UIColor.redColor()
+        case 1:
+            self.view.backgroundColor = UIColor.blueColor()
+        case 2:
+            self.view.backgroundColor = UIColor.greenColor()
+        default:
+            self.view.backgroundColor = UIColor.redColor()
+    }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        let tipPercentages = [0.1, 0.15, 0.2]
+        
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let billAmount = Double((billField.text! as NSString).doubleValue)
+        
+        let tip = billAmount * tipPercentage
+        
+        let total = billAmount + tip
+        
+   
+        tipLabel.text = String(format:"$%.2f", tip)
+        
+        totalLabel.text = String(format:"$%.2f", total)
+    }
+    
+   
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    
+    @IBAction func btnSave(sender: UIButton) {
+        let billAmount = Double((billField.text! as NSString).doubleValue)
+        let tipPercentages = [0.1, 0.15, 0.2]
+
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        let index = tipControl.selectedSegmentIndex
+        
+        
+        //Save
+        userDefaults.setObject(billAmount, forKey: "bill")
+        userDefaults.setObject(tipPercentage, forKey: "tip")
+        userDefaults.setInteger(index, forKey: "ind")
+        userDefaults.synchronize()
+        
+    }
+    
+    //VIEW CONTROLLER LIFE CYCLE
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+        
+        //Load new Percentage
+        let tipPercentage: Double! = (userDefaults.objectForKey(("tip"))!.doubleValue) as Double
+        let tipIndex = (userDefaults.integerForKey("ind"))
+        
+        
+        let billAmount = Double((billField.text! as NSString).doubleValue)
+        
+        
+        
+        tipControl.selectedSegmentIndex = tipIndex
+        
+        let tip = billAmount * tipPercentage
+        
+        let total = billAmount + tip
+       
+        tipLabel.text = String(format:"$%.2f", tip)
+        
+        totalLabel.text = String(format:"$%.2f", total)
+        
+       
+        //Load new theme
+        let bgColorNo = (userDefaults.integerForKey("bgColour"))
+        switch (bgColorNo) {
+        case 0:
+            self.view.backgroundColor = UIColor.redColor()
+        case 1:
+            self.view.backgroundColor = UIColor.blueColor()
+        case 2:
+            self.view.backgroundColor = UIColor.greenColor()
+        default:
+            self.view.backgroundColor = UIColor.redColor()
+        }
+
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+        
+        
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+        let billAmount = Double((billField.text! as NSString).doubleValue)
+        let tipPercentages = [0.1, 0.15, 0.2]
+        
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        let index = tipControl.selectedSegmentIndex
+        
+        
+        //Save
+        userDefaults.setObject(billAmount, forKey: "bill")
+        userDefaults.setObject(tipPercentage, forKey: "tip")
+        userDefaults.setInteger(index, forKey: "ind")
+        userDefaults.synchronize()
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("view did disappear")
+   
+    }
+    
+}
+
